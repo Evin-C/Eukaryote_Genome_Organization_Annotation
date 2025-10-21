2bcde94efa6fea31870a544dc0812942f3b39597
Evin Capan <145271800+Evin-C@users.noreply.github.com>
2025-10-22 01:54:23 +0200

    changed file name

---
+++ scripts/02_TEsorter_ltr_full.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=TEsorter           # Job name
+#SBATCH --output=TEsorter_%j.out      # Standard output log
+#SBATCH --error=TEsorter_%j.err       # Standard error log
+#SBATCH --cpus-per-task=16             
+#SBATCH --mem=80G                     # 80G memory allocation
+#SBATCH --time=24:00:00
+#SBATCH --partition=pibu_el8
+
+# Define variables
+USER="ecapan"
+WORKDIR="/data/users/${USER}/genome_organization_annotation/Eukaryote_Genome_Organization_Annotation"
+CONTAINER="/data/courses/assembly-annotation-course/CDS_annotation/containers/TEsorter_1.3.0.sif"
+INPUT_FILE="${WORKDIR}/EDTA_results/hifi_assembly.fa.mod.EDTA.raw/hifi_assembly.fa.mod.LTR.raw.fa"
+OUTDIR="${WORKDIR}/TEsorter_results/"
+
+# Create Output directory if needed
+mkdir -p "${OUTDIR}"
+cd "${OUTDIR}"
+
+# Run TEsorter
+apptainer exec --bind "${WORKDIR}" "${CONTAINER}" TEsorter \
+"${INPUT_FILE}" \
+-db rexdb-plant \
+-p "${SLURM_CPUS_PER_TASK}"
